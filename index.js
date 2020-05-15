fb63e6be29daf1b850018341811539cf05cd30c7
Gerben Aaltink <gerbenaaltink@gmail.com>
2020-05-15 20:08:54 +0200

    Updated connect

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const TableProxy = require('./proxy')
  * @param {string} name optional. :memory: is default if none given
  * @returns new Alopex instance
  */
-async function connect(name){
+function connect(name){
     let dbName = name || ':memory:'
-    const db = await Database.open(dbName)
-    db.dbName = dbName
-    return new Proxy(db, TableProxy)
+    return Database.open(dbName).then((db)=>{
+        db.dbName = dbName
+        return new Proxy(db, TableProxy)
+    })
 }
 
 module.exports = connect
